0dfc1b8468c33eb71a8c42ae5fb1dff085a59173
taosiqi <siqijson@gmail.com>
2025-06-25 23:15:54 +0800

    feat: 更新退款详情对话框宽度至900px

--- src/views/pay/refund/RefundDetail.vue
+++ src/views/pay/refund/RefundDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <Dialog v-model="dialogVisible" title="详情" width="700px" is-center>
+  <Dialog v-model="dialogVisible" title="详情" width="900px" is-center>
     <el-descriptions :column="2" label-class-name="desc-label">
       <el-descriptions-item label="商户退款单号">
         <el-tag size="small">{{ refundDetail.merchantRefundId }}</el-tag>

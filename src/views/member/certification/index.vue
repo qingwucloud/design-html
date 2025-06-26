a5e92b0cf83c51592ea3a4b6b3da3a93cf429893
taosiqi <siqijson@gmail.com>
2025-06-26 22:23:51 +0800

    feat: 添加合同数量列至表格

--- src/views/member/certification/index.vue
+++ src/views/member/certification/index.vue
@@ -147,6 +147,7 @@
         :formatter="dateFormatter"
         width="180px"
       />
+      <el-table-column label="合同数量" align="center" prop="contractCount" />
       <el-table-column label="认证状态" align="center" prop="certStatus">
         <template #default="scope">
           <el-tag v-if="scope.row.certStatus === 1" type="primary" size="small">已通过</el-tag>
